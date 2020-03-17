e9d9c04fd7460842b7d6bfcb449af39e052e8b75
Sam <sam.watemus@yandex.ru>
2020-03-17 22:16:49 +0300

    Add help form

---
+++ help.pas
@@ -0,0 +1,26 @@
+unit help;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TFormHelp = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  FormHelp: TFormHelp;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
